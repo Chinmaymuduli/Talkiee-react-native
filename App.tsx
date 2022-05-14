22440559e890a73030910e6891f84f6c8cabd4b1
Chinmaymuduli <chinmaymuduli0@gmail.com>
2022-05-14 15:56:28 +0530

    icons and fontFamily

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
-  return <Text>Hello welcome</Text>;
+  return (
+    <>
+      <Text style={{fontFamily: 'Nunito-Bold'}}>Hello welcome</Text>
+      <Ionicons name="menu" size={30} color="red" />
+    </>
+  );
 };
 
 export default App;
