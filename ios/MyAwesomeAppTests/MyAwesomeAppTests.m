af67da12b315c156d16104ed2603d5fb22753c26
Loushik <loushikkumargiri@gmail.com>
2022-05-15 16:39:13 +0530

    adding things

--- ios/MyAwesomeAppTests/MyAwesomeAppTests.m
+++ ios/MyAwesomeAppTests/MyAwesomeAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAwesomeAppTests : XCTestCase
+@interface talkieeTests : XCTestCase
 
 @end
 
-@implementation MyAwesomeAppTests
+@implementation talkieeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
